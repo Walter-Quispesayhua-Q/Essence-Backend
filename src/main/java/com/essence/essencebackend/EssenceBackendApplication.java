3a3efac1bf117d14266438820547de458dcefe72
Walter David Quispesayhua Quispe <walter.sayhua@gmail.com>
2025-12-27 12:00:06 -0500

    Iniciando con el proyecto

---
+++ src/main/java/com/essence/essencebackend/EssenceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.essence.essencebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssenceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EssenceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/essence/essencebackend/EssenceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essence.essencebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssenceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
